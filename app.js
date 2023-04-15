1435f80d72ba1f7a5202e53cfc10ad58ba4aef8d
XVI-IX <oladoja14@gmail.com>
2023-04-15 23:29:25 +0100

    Add Security

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// Extra Security
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 // connectDB
 const connectDB = require('./db/connect');
 const MONGO = process.env.MONGO_URI;
@@ -19,7 +25,16 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -21,6 +21,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.msg = `Duplicate value entered for ${err.keyValue} field`
     customError.statusCode = 400
   }
+
+  if (err.name === 'CastError') {
+    customError.message = `No item found with id  ${err.value}`;
+    customError.statusCode = 404;
+  }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({
     msg: customError.msg
