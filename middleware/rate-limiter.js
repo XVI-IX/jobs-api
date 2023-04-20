3da3fc3134cf80f050aa831476028d575cd9c70f
XVI-IX <oladoja14@gmail.com>
2023-04-20 12:32:02 +0100

    Update rate-limter.js to use asynchronous functions

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,66 +1,65 @@
-const Redis = require('redis');
-const redisClient = Redis.createClient({ legacyMode: true });
-
-redisClient.connect();
-
-const { APIAccessError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
+const Redis = require('ioredis');
 
+const redis = new Redis();
 
-const limiter = (req, res, next) => {
-  const authHeader = String(req.headers.authorization);
-  redisClient.exists(authHeader, (err, reply) => {
-    if (err) {
-      res.status(StatusCodes.BAD_GATEWAY).json({
-        error: 1,
-        msg: "Redis could not connect"
-      });
-    }
-    if (reply === 1) {
-      console.log(reply);
-      redisClient.get(authHeader, (err, reply) => {
-        let data = JSON.parse(reply);
-        console.log(data);
-        let currentTime = new Date().getTime();
-        let timeDiff = (currentTime - data.startTime) / 60000;
+const limiter = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  await redis.exists(authHeader)
+        .then(
+          async (result) => {
+          if (result === 1) {
+            console.log(result);
+            await redis.get(authHeader)
+                  .then(
+                    (result) => {
+                      let data = JSON.parse(result);
+                      console.log(data);
+                      let currentTime = new Date().getTime();
+                      let timeDiff = (currentTime - data.startTime) / 60000;
 
-        if (timeDiff >= 1) {
+                      if (timeDiff >= 1) {
+                        let body = {
+                          count: 1,
+                          startTime: new Date().getTime()
+                        }
 
-          // reset data
-          let body = {
-            count: 1,
-            startTime: new Date().getTime()
+                        redis.set(authHeader, JSON.stringify(body));
+
+                        next();
+                      }
+                      if (timeDiff < 1) {
+                        if (data.count > 3) {
+                          res.status(StatusCodes.SERVICE_UNAVAILABLE).json({
+                            error: -1,
+                            msg: "API access limit exceeded"
+                          })
+                        }
+
+                        data.count++;
+                        redis.set(authHeader, JSON.stringify(data));
+
+                        next();
+                      }
+                    }
+                  )
+          } else {
+            let body = {
+              count: 1,
+              startTime: new Date().getTime()
+            }
+
+            redis.set(authHeader, JSON.stringify(body));
+
+            next();
           }
-
-          redisClient.set(authHeader, JSON.stringify(body));
-
-          next();
-        }
-        if (timeDiff < 1) {
-          if (data.count > 3) {
-            res.status(StatusCodes.SERVICE_UNAVAILABLE).json({
-              error: -1,
-              msg: "API access limit exceeded"
-            });
-          }
-
-          data.count++;
-          redisClient.set(req.headers.authorization, JSON.stringify(data));
-
-          next();
-        }
-      });
-    } else {
-      let body = {
-        count: 1,
-        startTime: new Date().getTime(),
-      }
-
-      redisClient.set(authHeader, JSON.stringify(body));
-
-      next();
-    }
-  });
+        },
+        (err) => {
+          res.status(StatusCodes.BAD_GATEWAY).json({
+            error: 1,
+            msg: "Redis could not connect"
+          });
+        });
 }
 
 module.exports = limiter;
